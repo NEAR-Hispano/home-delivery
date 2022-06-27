978019f1184f58fef1d91552dc51644fea4bfa94
detextre4 <detextre4@gmail.com>
2022-06-27 11:46:53 -0400

    structure to save location + unctions to save schedule and others ( by cesar )

--- backend/contract/src/lib.rs
+++ backend/contract/src/lib.rs
@@ -1,11 +1,11 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen, AccountId, PanicOnDefault, Balance};
+use near_sdk::{env, near_bindgen, AccountId, Balance};
 use near_sdk::collections::{ UnorderedMap};
-//use near_sdk::json_types::{U128};
+use near_sdk::json_types::{U128};
 use serde::Serialize;
 use serde::Deserialize;
 use std::collections::HashMap;
-use near_sdk::json_types::ValidAccountId;
+// use near_sdk::json_types::ValidAccountId;
 //use near_sdk::env::is_valid_account_id;
 
 
@@ -15,8 +15,11 @@ near_sdk::setup_alloc!();
 #[serde(crate = "near_sdk::serde")]
 //structs for Stores
 pub struct StoreObject {
+    owner_id: AccountId,
     name: String,
     address: String,
+    location: String,
+    schedule:String,
     phone: String,
     wallet: String,
     logo: String,
@@ -31,6 +34,24 @@ pub struct StoreJson {
     wallet: String,
     logo: String,
 }
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
+//structs for Menu
+pub struct MenuObject {
+    id_tienda: AccountId,
+    platillos: Vec<PlatilloObject>,
+}
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
+//structs for Menu
+pub struct PlatilloObject {
+    id: i128,
+    name: String,
+    description: String,
+    category:String,
+    price: Balance,
+    img: String,
+}
 //structs for categories
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
 #[serde(crate = "near_sdk::serde")]
@@ -43,11 +64,14 @@ pub struct CategoriesJson {
     id: i128,
 	name: String,
 }
+
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
     stores: UnorderedMap<AccountId, StoreObject>,
+    menus: Vec<MenuObject>,
     categories: Vec<CategoriesJson>,
+    platillo_id: i128,
 }
 
 /// Initializing deafult impl
@@ -57,6 +81,8 @@ impl Default for Contract {
         Self {
             stores: UnorderedMap::new(b"s".to_vec()),
             categories: Vec::new(),
+            menus: Vec::new(),
+            platillo_id: 0,
         }
     }
 }
@@ -66,8 +92,11 @@ impl Default for Contract {
 impl Contract {
 //functions for stores
 pub fn set_store(&mut self, 
+    owner_id: AccountId,
     name: String,
     address: String,
+    location: String,
+    schedule: String,
     phone: String,
     wallet: String,
     logo: String,
@@ -77,34 +106,52 @@ pub fn set_store(&mut self,
         env::panic(b"store already exists");
     }
     let data = StoreObject {
+        owner_id: owner_id.to_string(),
         name: name,
         address: address,
+        location: location,
+        schedule:schedule,
         phone: phone,
         wallet: wallet,
         logo: logo,
     };
     self.stores.insert(&env::signer_account_id(), &data);
+
+    let data_menu = MenuObject {
+        id_tienda: env::signer_account_id().to_string(),
+        platillos: Vec::new(),
+    };
+    self.menus.push(data_menu);
     env::log(b"store Created");
     data
 }
 
 pub fn put_store(&mut self,
+    owner_id: AccountId,
     name: String,
     address: String,
+    location: String,
+    schedule: String,
     phone: String,
     wallet: String,
     logo: String,
 ) -> StoreObject {
     let return_data = StoreObject {
+        owner_id: owner_id.clone(),
         name: name.clone(),
         address: address.clone(),
+        location: location.clone(),
+        schedule:schedule.clone(),
         phone: phone.clone(),
         wallet: wallet.clone(),
         logo: logo.clone(),
     };
     let mut store = self.stores.get(&env::signer_account_id()).expect("Store does not exist");
+    store.owner_id = owner_id;
     store.name = name;
     store.address = address;
+    strore.location = location;
+    strore.schedule = schedule;
     store.phone = phone;
     store.wallet = wallet;
     store.logo = logo;
@@ -114,14 +161,46 @@ pub fn put_store(&mut self,
 }
 pub fn get_store(&self, user_id: AccountId) -> StoreObject {
     let store = self.stores.get(&user_id).expect("Store does not exist");
-
     StoreObject {
+        owner_id: store.owner_id,
         name: store.name,
         address: store.address,
         phone: store.phone,
         wallet: store.wallet,
         logo: store.logo,
     }
+}
+
+// funtions for get all stores
+pub fn get_all_stores(&self) -> Vec<StoreObject> {
+    self.stores.iter().map(|(_key, value)| value.clone()).collect()
+}
+
+// funtions for get all stores
+pub fn get_menu(&self, user_id: AccountId) -> MenuObject {
+    let index = self.menus.iter().position(|x| x.id_tienda == user_id.to_string()).expect("Menu no exists");
+    self.menus[index].clone()
+}
+// funtions for platillos
+pub fn set_platillo(&mut self,
+    name: String,
+    description: String,
+    category:String,
+    price: U128,
+    img: String,
+) -> MenuObject {
+    let index = self.menus.iter().position(|x| x.id_tienda == env::signer_account_id()).expect("Menu no exists");
+    self.platillo_id += 1;
+    self.menus[index].platillos.push(PlatilloObject {
+        id: self.platillo_id,
+        name: name,
+        description: description,
+        category: category,
+        price: price.0,
+        img: img,
+    });
+    env::log(b"Menu Created");
+    self.menus[index].clone()
 }
 
 // functions for categories
